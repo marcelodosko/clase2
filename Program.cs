35c45147f1a9a909e440bc322ef3216f2bb4a505
Marcelo Dosko <marcelodosko@gmail.com>
2021-08-24 20:42:38 -0300

    lo que sea

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace clase2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
